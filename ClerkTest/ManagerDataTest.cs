9151491f344f4b0c124ca1b5370e65a9126c59a8
Alejandro Huerta <1011415Huerta@gmail.com>
2016-05-14 08:58:25 -0700

    Rename test class

--- ClerkTest/ManagerDataTest.cs
+++ ClerkTest/ManagerDataTest.cs
@@ -6,7 +6,7 @@ using Newtonsoft.Json.Linq;
 
 namespace ClerkTest {
     [TestClass]
-    public class ManagerTest {
+    public class ManagerDataTest {
 
         [TestMethod]
         public void GetAValue() {
