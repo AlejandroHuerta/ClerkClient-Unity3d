9fec2e7c889145c074a055601267e51104d95222
Alejandro Huerta <1011415huerta@gmail.com>
2016-06-28 19:43:08 -0700

    Switch unix utils to milliseconds and add test

--- Clerk/Utils.cs
+++ Clerk/Utils.cs
@@ -6,22 +6,22 @@ namespace Clerk {
         /// <summary>
         /// Convert Unix time value to a DateTime object.
         /// </summary>
-        /// <param name="unixtime">The Unix time stamp you want to convert to DateTime.</param>
+        /// <param name="unixtime">The Unix time stamp, in milliseconds, you want to convert to DateTime.</param>
         /// <returns>Returns a DateTime object that represents value of the Unix time.</returns>
         public static DateTime UnixTimeToDateTime(long unixtime) {
             DateTime sTime = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
-            return sTime.AddSeconds(unixtime);
+            return sTime.AddMilliseconds(unixtime);
         }
 
         /// <summary>
         /// Convert a date time object to Unix time representation.
         /// </summary>
-        /// <param name="datetime">The datetime object to convert to Unix time stamp.</param>
+        /// <param name="datetime">The datetime object to convert to Unix time stamp, in milliseconds.</param>
         /// <returns>Returns a numerical representation (Unix time) of the DateTime object.</returns>
         public static long ConvertToUnixTime(this DateTime datetime) {
             DateTime sTime = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
 
-            return (long)(datetime - sTime).TotalSeconds;
+            return (long)(datetime - sTime).TotalMilliseconds;
         }
     }//Utils
 }
---
+++ ClerkTest/UtilsTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ClerkTest {
+    /// <summary>
+    /// Summary description for UtilsTest
+    /// </summary>
+    [TestClass]
+    public class UtilsTest {
+        [TestMethod]
+        public void UnixTimeToDateTime() {
+            var date = new DateTime(2016, 2, 26, 0, 17, 6, DateTimeKind.Utc);
+            Assert.AreEqual(date, Clerk.Utils.UnixTimeToDateTime(1456445826000));
+        }
+    }
+}
